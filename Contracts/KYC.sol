9215ef81d109edb6cbe16a42b92e080f685f80aa
thStormKing <ss4kakarot4@gmail.com>
2022-12-18 23:58:56 +0530

    Bank Interface completed

--- Contracts/KYC.sol
+++ Contracts/KYC.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
 
-contract customer{
+contract KYC{
     // Data structure to hold customer information
     struct Customer {
         string username;
@@ -12,7 +12,37 @@ contract customer{
         address Bank;
     }
 
-    mapping(string=>Customer) public customers;
+    //  Data structure to hold bank information
+    struct Bank{
+        string name;
+        address ethAddress;
+        uint256 complaintsReported;
+        uint256 KYC_count;
+        bool isAllowedToVote;
+        string regNumber;
+    }
+
+    //  Data structure to hold KYC information
+    struct KYC_request{
+        string username;
+        address bankAddress;
+        string customerData;
+        uint256 kycList_index;
+    }
+
+    // Mappings to store and retrieve data from Customer, Bank and KYC_request
+    mapping(string=>Customer) customers;
+    mapping(address=>Bank) banks;
+    mapping(string=>KYC_request) kyc_request;
+
+    // List of KYC_requests
+    KYC_request[] kycList;
+    
+    // Index for Kyc list
+    uint256 kyc_index;
+
+    // Counter for number of banks registered
+    uint256 bankCounter;
 
     // Modifier continue if the customer is not there
     modifier noCustomers(string memory _username){
@@ -26,20 +56,49 @@ contract customer{
         _;
     }
 
+    // Modifier to check if a bank can vote
+    modifier allowVote(){
+        require(banks[msg.sender].isAllowedToVote,"You are not allowed to vote");
+        _;
+    }
+
+    // Modifier for Bank only operations
+    modifier isBank(address bnkAddress){
+        require(banks[bnkAddress].ethAddress != address(0),"Only Bank can perform this operation");
+        _;
+    }
+
+    // Modifier for Admin only operations
+    modifier isAdmin(address sender){
+        require(admin == sender,"Only Admin can perform this operation");
+        _;
+    }
+
+    address admin;
+
+    // Constructor to set the admin
+    constructor(){
+        // Set admin address
+        admin = msg.sender;
+
+        // Initialise KYC index and bank counter to 0
+        kyc_index = 0;
+        bankCounter = 0;
+    }
 
     // Function to add customer
-    function addCustomer(string memory _username, string memory _customerData, address sender) public noCustomers(_username){        
+    function addCustomer(string memory _username, string memory _customerData) public noCustomers(_username) isBank(msg.sender){        
         // create customer record in struct
         customers[_username].username = _username;
         customers[_username].customerData = _customerData;
-        customers[_username].Bank = sender;
+        customers[_username].Bank = msg.sender;
         customers[_username].kycStatus = false;
         customers[_username].Upvotes = 0;        
         customers[_username].Downvotes = 0;        
     }
 
     // Function to view customer
-    function viewCustomer(string memory _username) public view onlyCustomers(_username) returns(string memory, string memory, address){
+    function viewCustomer(string memory _username) public view onlyCustomers(_username)  isBank(msg.sender) returns(string memory, string memory, address){
         return (
             customers[_username].username,
             customers[_username].customerData,
@@ -48,115 +107,83 @@ contract customer{
     }
 
     // Function to modify customer information
-    function modifyCustomer(string memory _username, string memory _customerData) public onlyCustomers(_username){
-        customers[_username].customerData = _customerData;
+    function modifyCustomer(string memory _username, string memory _customerData) public onlyCustomers(_username) isBank(msg.sender){
+        removeRequest(_username);
+        Customer storage c = customers[_username];
+        c.customerData=_customerData;
+        c.Upvotes=0;
+        c.Downvotes=0;
+        c.Bank=msg.sender;
     }
 
     // Function to upvote customer
-    function upVote(string memory _username)public onlyCustomers(_username){
+    function upVote(string memory _username) internal onlyCustomers(_username) allowVote isBank(msg.sender){
         customers[_username].Upvotes++;
     }
     // Function to downvote customer
-    function downVote(string memory _username)public onlyCustomers(_username){
-        customers[_username].Downvotes++;
-    }
-}
-
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.17;
-
-import {customer} from "./customer.sol";
-import {kyc_request} from "./kyc_request.sol";
-
-contract bank{
-    //  Data structure to hold bank information
-    struct Bank{
-        string name;
-        address ethAddress;
-        uint256 complaintsReported;
-        uint256 KYC_count;
-        bool isAllowedToVote;
-        string regNumber;
-    }
-
-    // Mapping to uniquely identify bank by the address
-    mapping(address => Bank) banks;
-
-    // Mapping referring back to the Customer struct
-    // Customer object creation
-    mapping(string=>customer.Customer) public customers;
-    
-    // KYC object creation
-    mapping(string=>kyc_request.kyc) public kyc;
-
-    // Modifier to check if customer exists
-    modifier onlyCustomer(string memory _username){
-        require(customers[_username].Bank != address(0),"Customer does not exist");
-        _;
-    }
-
-    modifier allowVote(){
-        require(banks[msg.sender].isAllowedToVote,"You are not allowed to vote");
-        _;
-    }
-
-    // Create reference customer_ref to contract customer
-    customer public customer_ref;
-    kyc_request public kyc_ref;
-
-    // Bank constructor
-    constructor(customer _addressCustomerContract, kyc_request _addressKycContract){
-        customer_ref = _addressCustomerContract;
-        kyc_ref = _addressKycContract;
-    }
-
-    // Call customer.sol add function
-    function addCustomer(string memory _username, string memory _customerData) public{
-        address sender = msg.sender;        
-        customer_ref.addCustomer(_username,_customerData,sender);
-    }
-
-    // Call customer.sol view function
-    function viewCustomer(string memory _username) public view returns(string memory, string memory, address){   
-        string memory username;
-        string memory customerData;
-        address bankAddress;
-
-        (username,customerData,bankAddress) = customer_ref.viewCustomer(_username);
-        return (username,customerData,bankAddress);
-    }
-
-    // Function to modify customer information
-    function modifyCustomer(string memory _username, string memory _customerData) public {
-        customer_ref.modifyCustomer(_username,_customerData);
-    }
-
-    // Function to upvote customer
-    function upVote(string memory _username)public allowVote{
-        customers[_username].Upvotes++;
-    }
-
-    // Function to downvote customer
-    function downVote(string memory _username)public allowVote{
+    function downVote(string memory _username) internal onlyCustomers(_username) allowVote isBank(msg.sender){
         customers[_username].Downvotes++;
     }
 
-    // Add KYC request
-    function addRequest(string memory _username, string memory _customerdata) public onlyCustomer(_username){        
+    // Function to add data to KYC request struct and kyc_list
+    function addRequest(string memory _username, string memory _customerdata) public onlyCustomers(_username) isBank(msg.sender){  
+        // Add details to the KYC_request struct
+        KYC_request storage kyc = kyc_request[_username];
+        kyc.username=_username;
+        kyc.customerData=_customerdata;
+        kyc.bankAddress=msg.sender;
+        kyc.kycList_index=kyc_index++;
+
+        // Push kyc request information to kyc request list
+        kycList.push(kyc);
+
+        // Increment bank KYC_count
+        banks[msg.sender].KYC_count++;
     }
-}
 
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.17;
+    // Remove KYC request
+    function removeRequest(string memory _username) internal onlyCustomers(_username)  isBank(msg.sender){
+        // Get the index of the kyc request
+        uint256 idx = kyc_request[_username].kycList_index;
 
-contract kyc_request{
-    //  Data structure to hold KYC information
-    struct kyc{
-        string username;
-        address bankAddress;
-        string customerData;
+        // Delete from KYC requests list. Not requests
+        delete kycList[idx];
     }
 
-    // Mapping to point to the struct using username
-    mapping(string=>kyc) public KYC;
+    // Get Bank Complaints
+    function getBankComplaint(address _bnk) public view  isBank(msg.sender) returns(uint256){
+        return banks[_bnk].complaintsReported;
+    }
+
+    // View Bank Details
+    function viewBankDetails(address _bnk) public view  isBank(msg.sender) returns(Bank memory){
+        return banks[_bnk];
+    }
+
+    // Report Bank
+    function reportBank(address _bnkAddress, string memory _name) public {
+        Bank storage b = banks[_bnkAddress];
+        require(b.ethAddress==_bnkAddress,"Bank address not found");
+        string memory b_name = b.name;
+
+        // Convert strings bank name and input name to bytes32 for easy comparison
+        bytes32 bankName;
+        assembly {
+            bankName := mload(add(b_name,32))
+        }
+        bytes32 inName;
+        assembly {
+            inName := mload(add(_name,32))
+        }
+
+        require(bankName == inName,"Bank name and address do not match");
+        b.complaintsReported++;
+
+        validBank(_bnkAddress);
+    }
+
+    // Function to modify isAllowedToVote
+    function validBank(address _bnkAddress) public {
+
+    }
 }
